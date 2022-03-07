9b51e49ac65560d13ec99c5d8cca60ca73d56510
cf <carter@carter.at>
2022-03-07 20:44:34 +0800

    new

--- go.mod
+++ go.mod
@@ -1,3 +1,19 @@
 module github.com/zhongshuwen/zswchain-go-demo
 
 go 1.15
+
+require (
+	contrib.go.opencensus.io/exporter/stackdriver v0.13.10 // indirect
+	github.com/BurntSushi/toml v1.0.0 // indirect
+	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
+	github.com/streamingfast/logging v0.0.0-20220304214715-bc750a74b424 // indirect
+	github.com/teris-io/shortid v0.0.0-20201117134242-e59966efd125 // indirect
+	github.com/tidwall/gjson v1.14.0 // indirect
+	github.com/zhongshuwen/zswchain-go v0.3.0 // indirect
+	go.uber.org/atomic v1.9.0 // indirect
+	go.uber.org/multierr v1.8.0 // indirect
+	golang.org/x/sys v0.0.0-20220227234510-4e6760a101f9 // indirect
+	golang.org/x/term v0.0.0-20210927222741-03fcf44c2211 // indirect
+	golang.org/x/tools v0.1.9 // indirect
+	honnef.co/go/tools v0.2.2 // indirect
+)
--- main.go
+++ main.go
@@ -2,11 +2,45 @@ package main
 
 import (
 	"fmt"
-	"runtime"
+	"os"
+	"context"
+	"encoding/json"
+
+	zsw "github.com/zhongshuwen/zswchain-go"
 )
 
 var version = "dev"
 
+
+
+func NoError(err error, message string, args ...interface{}) {
+	if err != nil {
+		Quit(message+": "+err.Error(), args...)
+	}
+}
+
+func Quit(message string, args ...interface{}) {
+	fmt.Printf(message+"\n", args...)
+	os.Exit(1)
+}
+
+
 func main() {
-	fmt.Println("hello world, git! (", version, runtime.GOOS, runtime.GOARCH, ")")
+	api := zsw.New("http://localhost:3031")
+	ctx := context.Background()
+
+	infoResp, err := api.GetInfo(ctx)
+	NoError(err, "unable to get chain info")
+
+	fmt.Println("Chain Info", toJson(infoResp))
+
+	accountResp, _ := api.GetAccount(ctx, "zsw.admin")
+	fmt.Println("Account Info", toJson(accountResp))
+}
+
+func toJson(v interface{}) string {
+	out, err := json.MarshalIndent(v, "", "  ")
+	NoError(err, "unable to marshal json")
+
+	return string(out)
 }
